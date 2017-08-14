82b595076ca35aea5ff27194fa82ecb0242c40ec
Jens Reimann <jreimann@redhat.com>
2017-08-14 14:15:29 +0200

    Set retention and target for topic annotation

--- asyncapi/src/main/java/de/dentrassi/asyncapi/Topic.java
+++ asyncapi/src/main/java/de/dentrassi/asyncapi/Topic.java
@@ -16,6 +16,13 @@
 
 package de.dentrassi.asyncapi;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Topic {
     public String name();
 
